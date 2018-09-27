3e1dac6b2b29ca2ae6c5f153a16ffefed292d2e4
luciliah <luciliamandlatinha@gmail.com>
2018-09-28 00:22:46 +0200

    navbar

--- gassx/resources/js/components/Navbar.vue
+++ gassx/resources/js/components/Navbar.vue
@@ -1,14 +1,30 @@
 <template>
-    <div class=" header clearfix">
-        <nav>
-            <ul class="nav nav-pills pull-right"><li role ="presentation" class="active"><a href="#"></a>Home</a></li><li role ="presentation"><a href="#"></a>About</a></li>
-                <li role ="presentation"><a href="#"></a>Contact</a></li>
+   
+   	<div class=" header clearfix">
+	    <nav>
 
-            </ul>
-            <h3 class="text-muted"> project name</h3>
-        </nav>
+	    	<ul class="nav  justify-content-end">
+				<li class="nav-item">
+				    <a class="nav-link active" href="#">Inicio</a>
+				</li>
+				<li class="nav-item">
+				    <a class="nav-link" href="#">Parceiros</a>
+				</li>
+				<li class="nav-item">
+				    <a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+				    <a class="nav-link " href="#">Contactos</a>
+				</li>
+
+			</ul>
+		
+	   </nav>
+	   <h3 class="text-muted"> GassX</h3>
+	</div> 
+
         
-    </div>
+   
 </template>
 <script>
     
