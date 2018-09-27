e57f41464cdb4f813f58d22b178cf1272dffcc58
luciliah <luciliamandlatinha@gmail.com>
2018-09-27 22:01:36 +0200

    navbar
    bug resolvido

--- gassx/resources/js/app.js
+++ gassx/resources/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('CcNavbar', require('./components/Navbar.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ gassx/resources/js/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class=" header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right"><li role ="presentation" class="active"><a href="#"></a>Home</a></li><li role ="presentation"><a href="#"></a>About</a></li>
+                <li role ="presentation"><a href="#"></a>Contact</a></li>
+
+            </ul>
+            <h3 class="text-muted"> project name</h3>
+        </nav>
+        
+    </div>
+</template>
+<script>
+    
+</script>
+
+<style>
+    
+</style>
