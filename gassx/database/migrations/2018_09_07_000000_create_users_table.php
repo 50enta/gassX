dc9af1803ee99344a93c5788df4472bad395d719
50enta <valtercinquenta@outlook.com>
2018-10-05 14:47:58 -1200

    descomentando atributos do migrate user

--- gassx/database/migrations/2018_09_07_000000_create_users_table.php
+++ gassx/database/migrations/2018_09_07_000000_create_users_table.php
@@ -15,24 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            // $table->boolean('activo')->nullable();
-            // $table->string('codigo');
+            $table->boolean('activo')->nullable();
+            $table->string('codigo');
             $table->string('name');
             $table->string('email')->unique();
-            //  $table->string('status');
-            //  $table->string('foto');
-            // $table->double('saldo');
+            $table->string('status');
+            $table->string('foto');
+            $table->double('saldo');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             
-            // $table->integer('contacto_id')->unsigned();
-            // $table->integer('genero_id')->unsigned();
-            // $table->integer('endereco_id')->unsigned();
+            $table->integer('contacto_id')->unsigned();
+            $table->integer('genero_id')->unsigned();
+            $table->integer('endereco_id')->unsigned();
             $table->timestamps();
-            // $table->foreign('contacto_id')->references('id')->on('contactos')->onDelete('cascade');
-            // $table->foreign('genero_id')->references('id')->on('generos')->onDelete('cascade');
-            // $table->foreign('endereco_id')->references('id')->on('enderecos')->onDelete('cascade');
+            $table->foreign('contacto_id')->references('id')->on('contactos')->onDelete('cascade');
+            $table->foreign('genero_id')->references('id')->on('generos')->onDelete('cascade');
+            $table->foreign('endereco_id')->references('id')->on('enderecos')->onDelete('cascade');
         });
     }
 
