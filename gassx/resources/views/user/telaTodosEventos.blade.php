c7600630bb788ca62f127095976e3ae0a29f5ba3
luciliah <luciliamandlatinha@gmail.com>
2018-10-19 08:12:30 +0200

    bbbb

--- gassx/resources/views/user/telaTodosEventos.blade.php
+++ gassx/resources/views/user/telaTodosEventos.blade.php
@@ -5,25 +5,27 @@
 
  <div class="content">
 	<div class="container-fluid">
-
-
-                          <!-- Page-Title -->
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="page-title-box">
-                                    <h4 class="page-title">Todos eventos</h4>
-                                    <form class="form-horizontal float-right" role="form" method="POST" action="{{ url('/user/atualizarTodosEventos/'.$dados['usuario']->id) }}">
-										{{csrf_field() }}
-										<div class="form-group row">
-										    <input name="mes_ano" type="text" class="col-sm-5 form-control form-control-1 input-sm from" placeholder="Mês e ano" >
-										    <button type="submit" class="btn btn-success waves-effect waves-light btn-sm m-b-5">Buscar</button>
-										</div>
-									</form>
-                                    <div style="padding-bottom: 18px;"class=""></div>
-                                </div>
-                            </div>
-                        </div>
-
+<!-- <br>
+<br>
+<br>
+<br> -->
+                        <!-- Page-Title -->
+        <div class="row">
+                 <div class="col-sm-12">
+                     <div class="page-title-box">
+                         <h4 class="page-title">Todos Eventos</h4>
+                        <form class="form-horizontal float-right" role="form" method="POST" action="{{ url('') }}">
+							{{csrf_field() }}
+                                 
+                             <div class="form-group row">
+								<input name="mes_ano" type="text" class="col-sm-5 form-control form-control-1 input-sm from" placeholder="Mês e ano" >
+								<button type="submit" class="btn btn-success waves-effect waves-light btn-sm m-b-5" >Buscar</button>
+							</div>
+						</form>
+                   		 <div style="padding-bottom: 18px;"class=""></div>
+                   </div>
+                </div>
+            </div>
 
 
                          
@@ -40,8 +42,7 @@
 			              	</div>
 			                    <img class="img-fluid" src="{{asset('minton/images/small/img1.jpg')}}" alt="Card image cap">
 			                <div class="card-body">
-			                    <p class="card-text">Some quick example text to build on the card title and make
-			                            up the bulk of the card's content.</p>
+			                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 			                            
 			                    
 			                     <a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-1" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-1']);">Participar</a>
@@ -63,10 +64,10 @@
 			            </div>
 			                    <img class="img-fluid" src="{{asset('minton/images/small/img2.jpg')}}" alt="Card image cap">
 			            <div class="card-body">
-			                    <p class="card-text">Some quick example text to build on the card title and make
-			                            up the bulk of the card's content.</p>
-			                <a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-1']);">Participar</a>
-			                    <a href="{{url('/eventos/todosEventos/telaDetalhes')}}" class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> mais detalhes</a>
+			                <p class="card-text">Some quick example text to build on the card title and make
+			                 up the bulk of the card's content.</p>
+			                <a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-4']);">Participar</a>
+			                <a href="{{url('/eventos/todosEventos/telaDetalhes')}}" class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> mais detalhes</a>
 			            </div> <!-- end card-body-->
 			        </div> <!-- end card-->
 			    </div><!-- end col -->
@@ -80,10 +81,9 @@
 			              	</div>
 			                    <img class="img-fluid" src="{{asset('minton/images/small/img5.jpg')}}" alt="Card image cap">
 			                <div class="card-body">
-			                    <p class="card-text">Some quick example text to build on the card title and make
-			                            up the bulk of the card's content.</p>
+			                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 			                    
-										<a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-1']);">Participar</a>
+								<a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-4']);">Participar</a>
 			                    <a href="{{url('/eventos/todosEventos/telaDetalhes')}}" class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> mais detalhes</a>
 			                    
 			                </div> <!-- end card-body-->
@@ -206,29 +206,24 @@
 
      document.querySelector('.sweet-4').onclick = function(){
 		swal({
-			 	title: "Are you sure?",
-			  text: "You will not be able to recover !",
+			 	title: "Tem Certeza?",
+			  text: "se escolher Sim ira se tornar participante deste Evento !",
 			  type: "warning",
 			  showCancelButton: true,
 			  confirmButtonClass: "btn-danger",
 			  confirmButtonText: "Sim!",
-			  cancelButtonText: "Nao, cancel!",
+			  cancelButtonText: "Nao, cancela!",
 			  closeOnConfirm: false,
 			  closeOnCancel: false
 		},
 		function(isConfirm) {
-		  if (isConfirm) {
-		    swal({
-			  title: "Sweet!",
-			  text: "Here's a custom image.",
-			  imageUrl: 'thumbs-up.jpg'
-			});
-		  } else {
-		    swal("Cancelled", "Your imaginary file is safe :)", "error");
-		  }
+  			if (isConfirm) {
+    			swal("Agora é Participante do evento");
+  			} else {
+    			swal("Cancelled", "Your imaginary file is safe :)", "error");
+  			}
 		});
-     };
-
+	};
 </script>
 
 </div>
