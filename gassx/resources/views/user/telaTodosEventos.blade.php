72b18046dbe9f96591e6be009a7ff5c33c7724bf
luciliah <luciliamandlatinha@gmail.com>
2018-10-19 10:42:20 +0200

    Update gassx/resources/views/user/telaTodosEventos.blade.php

--- gassx/resources/views/user/telaTodosEventos.blade.php
+++ gassx/resources/views/user/telaTodosEventos.blade.php
@@ -47,7 +47,7 @@
 			                    
 			                    <!--  <a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-1" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-1']);">Participar</a> -->
 
-			                    <button class="btn btn-primary sweet-1" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-43']);">Try It</button>
+			                    <button class="btn btn-primary sweet-1" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-1']);">Try It</button>
 
 			                    <a href="{{url('/eventos/todosEventos/telaDetalhes')}}"  class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline" data-target=""></i> mais detalhes</a>
 			                    
@@ -67,7 +67,7 @@
 			                <p class="card-text">Some quick example text to build on the card title and make
 			                 up the bulk of the card's content.</p>
 			                <!-- <a href="javascript: void(0)" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-4']);">Participar</a> -->
-			                <button class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-4']);">Try It</button>
+			                <button class="btn btn-primary sweet-2" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-2']);">Try It</button>
 			                <a href="{{url('/eventos/todosEventos/telaDetalhes')}}" class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> mais detalhes</a>
 			            </div> <!-- end card-body-->
 			        </div> <!-- end card-->
@@ -84,7 +84,7 @@
 			                <div class="card-body">
 			                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 			                    
-								<a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-4" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-42']);">Participar</a>
+								<a href="#" data-toggle="#con-close-modal" class="btn btn-primary sweet-2" onclick="_gaq.push(['_trackEvent', 'example', 'try', 'sweet-2']);">Participar</a>
 			                    <a href="{{url('/eventos/todosEventos/telaDetalhes')}}" class="card-link text-custom"><i class="mdi mdi-arrow-right-bold-hexagon-outline"></i> mais detalhes</a>
 			                    
 			                </div> <!-- end card-body-->
@@ -205,7 +205,28 @@
         swal("Here's a message!");
       };
 
-     document.querySelector('.sweet-4').onclick = function(){
+     document.querySelector('.sweet-2').onclick = function(){
+		swal({
+			 	title: "Tem Certeza?",
+			  text: "se escolher Sim ira se tornar participante deste Evento !",
+			  type: "warning",
+			  showCancelButton: true,
+			  confirmButtonClass: "btn-danger",
+			  confirmButtonText: "Sim!",
+			  cancelButtonText: "Nao, cancela!",
+			  closeOnConfirm: false,
+			  closeOnCancel: false
+		},
+		function(isConfirm) {
+  			if (isConfirm) {
+    			swal("Agora é Participante do evento");
+  			} else {
+    			swal("Cancelled", "Your imaginary file is safe :)", "error");
+  			}
+		});
+	};
+
+	document.querySelector('.sweet-2').onclick = function(){
 		swal({
 			 	title: "Tem Certeza?",
 			  text: "se escolher Sim ira se tornar participante deste Evento !",
