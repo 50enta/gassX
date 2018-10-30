ff8632f65152a19f2027bee959f604a9b395bf24
luciliah <luciliamandlatinha@gmail.com>
2018-10-30 08:09:29 +0200

    Update gassx/resources/views/auth/login.blade.php

--- gassx/resources/views/auth/login.blade.php
+++ gassx/resources/views/auth/login.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
+<div class="container widescreen">
+
+  <div class="wrapper-page">
+
+            <div class="text-center">
+                <!-- <a href="index.html" class="logo-lg"><i class="mdi mdi-radar"></i> <span>Minton</span> </a> -->
+                <br>
+                <br>
+                <h1  style=" color: #008cff">Gestão de associações </h1><br><br>
+            </div>
+
+           
+            <form class="form-horizontal m-t-20" method="POST" action="{{ route('login') }}">
+                <div class="form-group row">
+                    <div class="col-8 offset-2">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="mdi mdi-account"></i></span>
+
 <div class="container">
 
   <div class="row justify-content-center">
@@ -83,3 +102,68 @@
 
 
 @endsection
+
+
+ <!-- <form class="text-center" method="POST" action="{{ route('login') }}">
+                                     @csrf
+                                    <div class="form-group">
+                                        <label for="emailaddress">{{ __('Endereço e-mail') }}</label>
+                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                                    </div>
+
+                                    <div class="form-group">
+                                        
+                                        <label for="password">{{ __('Senha') }}</label>
+                                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? 'ínválido' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                                    </div>
+
+                                    <div class="form-group mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('memorizar') }}
+                                    </label>
+                                    <a href="pages-recoverpw.html" class="text-muted float-right"><small>Esqueceu a senha?</small></a>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-primary" type="submit"> Entrar </button>
+                                    </div>
+
+
+                                </form> -->
+
+                                </form>
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                        <div class="row mt-3">
+                    <div class="col-12 text-center">
+                        <p class="text-muted">Não tem conta? <a href="{{ route('register') }}" class="text-dark ml-1"><b>Registe-se</b></a></p>
+                   
+                    </div>  
+                </div>
+                    
+            </div> <!-- end col -->
+        </div>
+    </div>
+                
+
+
+@endsection
+
