91d2459f56bad92fd777ebdbebb3fc50b51b8e72
50enta <valtercinquenta@outlook.com>
2018-11-02 09:35:43 -1200

    acrecimo do atributo valor para gasto_user

--- gassx/database/migrations/2018_09_13_000532_create_gasto_users_table.php
+++ gassx/database/migrations/2018_09_13_000532_create_gasto_users_table.php
@@ -16,6 +16,7 @@ class CreateGastoUsersTable extends Migration
         Schema::create('gasto_users', function (Blueprint $table) {
             $table->increments('id');
             $table->boolean('ativo')->default('1');
+            $table->double('valor')->default(0);
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
