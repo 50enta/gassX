2aacd73ee56fed5d36d054ba137c7382f2fae902
luciliah <luciliamandlatinha@gmail.com>
2018-11-04 17:02:24 +0200

    server.php file

--- gassx/resources/views/Principal.blade.php
+++ gassx/resources/views/Principal.blade.php
@@ -268,12 +268,8 @@
             <div class="content-page">
 
 
-                <!--  <br><br><br><br>
-                @foreach($dados['data'] as $k)
-                        {{
-                           $k
-                    }} <br>
-                @endforeach     -->          
+                <br><br><br><br>
+                     
                 <!-- Start content -->
                      @yield('base', new Illuminate\Support\HtmlString(view('minton.inicio1')))
 
